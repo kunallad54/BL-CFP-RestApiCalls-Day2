0f1b15fe5d4e4be94c32e184422cf61caa865a1a
kunallad54 <kunallad5473@gmail.com>
2021-08-09 19:22:50 +0530

    Use Put Request Method and pass firstName as PathVariable and lastName as Query Parameter

--- restapicalls/src/main/java/com/bridgelabz/krunal/restapicalls/controller/HelloRestController.java
+++ restapicalls/src/main/java/com/bridgelabz/krunal/restapicalls/controller/HelloRestController.java
@@ -1,12 +1,10 @@
 /************************************************************************************************
  * 
- * Purpose : UC4 : Make REST Call to show Hello Krunal Lad Welcome to BridgeLabz
- * 			   	   - Use POST Request Method and pass first name and last name in the Body
- * 				   - Create User DTO Bean with firstName and lastName as attributes. 
- * 				   - Use CURL to demonstrate the REST API Call 
- * 				   - curl -X POST -H "Content-Type: application/json" -d 
- * 						'{"firstName": "Krunal","lastName": "Lad"}'
- * 				   - "http://localhost:8080/hello/post" -w "\n"
+ * Purpose : UC5 : Make REST Call to show Hello Krunal Lad Welcome to BridgeLabz
+ * 				   - Use PUT Request Method and pass first name as 
+ * 					 Path Variable and last name as Query Parameter
+ * 				   - Use CURL to demonstrate the REST API Call
+ * 				   - curl -X PUT localhost:8080/hello/put/Krunal/?lastName=Lad -w "\n"
  * 
  * @author Krunal Lad
  * @since 09-08-2021
@@ -17,6 +15,7 @@ package com.bridgelabz.krunal.restapicalls.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -71,9 +70,24 @@ public class HelloRestController {
 	 * @param userInfo
 	 * @return
 	 */
-	@PostMapping("/post")
+	@PostMapping("/hello/post")
 	public String sayHello(@RequestBody UserInfo userInfo) {
 		return "Hello " + userInfo.getFirstName() + userInfo.getLastName() + " Welcome to Bridgelabz";
+	}
+	
+	/**
+	 * Purpose : Annotation for mapping HTTP PUT requests onto specific handler methods.
+	 * 			 Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter.
+	 * 			 Once the mapping takes place in the server, the sayHello() is called and the output message is
+	 * 			 displayed as output.
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @return
+	 */
+	@PutMapping("/hello/put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam (value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + " Welcome to BridgeLabz";
 	}
 	
 }
