8dde4f512007f51ae9107e5f038256afa5378698
kunallad54 <kunallad5473@gmail.com>
2021-08-09 19:04:47 +0530

    Ability to use POST Request Method and pass first name and last name in the body

--- restapicalls/src/main/java/com/bridgelabz/krunal/restapicalls/controller/HelloRestController.java
+++ restapicalls/src/main/java/com/bridgelabz/krunal/restapicalls/controller/HelloRestController.java
@@ -1,9 +1,12 @@
 /************************************************************************************************
  * 
- * Purpose : UC3 : Make REST Call to show Hello Krunal Welcome to BridgeLabz
- * 				   - Use GET Request Method and pass name as path variable
- * 				   - Use CURL to demonstrate the REST API Call
- * 				   - curl localhost:8080/hello/param/Krunal -w "\n"
+ * Purpose : UC4 : Make REST Call to show Hello Krunal Lad Welcome to BridgeLabz
+ * 			   	   - Use POST Request Method and pass first name and last name in the Body
+ * 				   - Create User DTO Bean with firstName and lastName as attributes. 
+ * 				   - Use CURL to demonstrate the REST API Call 
+ * 				   - curl -X POST -H "Content-Type: application/json" -d 
+ * 						'{"firstName": "Krunal","lastName": "Lad"}'
+ * 				   - "http://localhost:8080/hello/post" -w "\n"
  * 
  * @author Krunal Lad
  * @since 09-08-2021
@@ -13,9 +16,13 @@ package com.bridgelabz.krunal.restapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.krunal.restapicalls.model.UserInfo;
 
 @RestController
 public class HelloRestController {
@@ -55,6 +62,18 @@ public class HelloRestController {
 		return  "Hello " + name + " Welcome to Bridelabz";
 	}
 	
-	
+	/**
+	 * Purpose : Annotation for mapping HTTP POST requests onto specific handler methods.
+	 * 			 Use POST Request Method and pass first name and last name in the Body.
+	 * 			 Once the mapping takes place in the server, the sayHello() is called 
+	 * 			 and the output message is displayed as output.
+	 * 
+	 * @param userInfo
+	 * @return
+	 */
+	@PostMapping("/post")
+	public String sayHello(@RequestBody UserInfo userInfo) {
+		return "Hello " + userInfo.getFirstName() + userInfo.getLastName() + " Welcome to Bridgelabz";
+	}
 	
 }
